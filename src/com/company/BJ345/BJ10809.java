733fdc41bc6c6aabac9b3033a1b4a02ec4275f5c
ymlee <joo0171@naver.com>
2021-01-04 23:04:49 +0900

    1/4

---
+++ src/com/company/BJ345/BJ10809.java
@@ -0,0 +1,7 @@
+package com.company.BJ345;
+
+public class BJ10809 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/company/BJ345/BJ2675.java
@@ -0,0 +1,25 @@
+package com.company.BJ345;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2675 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int num = Integer.parseInt(br.readLine()); //test case 개수
+
+
+        for(int i=0;i<num;i++){
+            st = new StringTokenizer(br.readLine());
+            int rep = Integer.parseInt(st.nextToken());
+            String string = st.nextToken();
+        }
+
+
+
+    }
+}
