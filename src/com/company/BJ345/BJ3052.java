6b52e8436251b7ae649add50c438cc749b6e5060
ymlee <joo0171@naver.com>
2021-01-11 20:09:20 +0900

    1/11

--- src/com/company/BJ345/BJ3052.java
+++ src/com/company/BJ345/BJ3052.java
@@ -1,10 +1,9 @@
 package com.company.BJ345;
 
-//실패!
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class BJ3052 {
     public static void main(String[] args) throws IOException {
@@ -12,10 +11,25 @@ public class BJ3052 {
 
         int arr[]=new int[10];
 
+        //나머지 값 배열에 넣는다.
         for(int i=0;i<10;i++){
             int num=Integer.parseInt(br.readLine());
             arr[i]=num%42;
         }
+
+        //배열 오름차순 정렬
+       Arrays.sort(arr);
+
+        int result = 0;
+        for(int i=0;i<10;i++){
+            if(i<9){
+                if(arr[i]!=arr[i+1]){
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result+1);
 
     }
 }
--- src/com/company/BJ345/BJ4344.java
+++ src/com/company/BJ345/BJ4344.java
@@ -1,7 +1,5 @@
 package com.company.BJ345;
 
-//실패!
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
